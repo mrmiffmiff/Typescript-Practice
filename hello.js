0e8774eff7375ca1b63cab0edc05c80ad330f419
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-10-19 19:37:08 -0700

    more complex hello world (with errors)

--- hello.js
+++ hello.js
@@ -1,2 +1,5 @@
-// Greets the world.
-console.log("Hello world!");
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date));
+}
+greet('Robert');
