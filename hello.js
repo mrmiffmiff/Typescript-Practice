f172532256bc685b982d4518f896c30d12920b2e
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-10-29 11:38:07 -0700

    target es6

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
 // This is an industrial-grade general-purpose greeter function:
 function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString()));
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 greet('Robert', new Date());
-var msg = "hello there";
+let msg = "hello there!";
 function obi() {
     console.log(msg);
 }
--- hello.ts
+++ hello.ts
@@ -1,11 +1,11 @@
 // This is an industrial-grade general-purpose greeter function:
 function greet(person: string, date: Date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}`);
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 
 greet('Robert', new Date());
 
-let msg = "hello there";
+let msg = "hello there!";
 
 function obi() {
     console.log(msg);
