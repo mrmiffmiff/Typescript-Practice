fe28ab247a4bc147819347cb9feec8e832055a29
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-03-17 13:11:44 -0700

    constraints on generics and common errors

--- functions.js
+++ functions.js
@@ -53,3 +53,39 @@ function map(arr, func) {
 const parsed = map(["1", "2", "3"], (n) => Number.parseInt(n));
 // I find this really cool how well the inference works tbh, the hints even changed as I typed that
 console.log(parsed);
+// Sometimes we may want to relate values like above but limit to a certain subset of types of values to operate on
+// Can use constraints to limit the types a type parameter accepts
+function longest(a, b) {
+    if (a.length >= b.length) { // because both a and b are of Type which *must* have a length, we can access the length; without that constraint we couldn't because there's always the possibility it won't exist
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+// longerArray is of type 'number[]'
+const longerArray = longest([1, 2], [1, 2, 3]);
+console.log(longerArray);
+// longerString is of type 'alice' | 'bob'
+const longerString = longest("alice", "bob");
+console.log(longerString);
+// This won't work and will error because Numbers don't have a 'length' property
+// const notOK = longest(10, 100);
+// Still inferred return types in each case
+// Common error:
+// function minimumLength<Type extends { length: number }>(
+//     obj: Type,
+//     minimum: number
+// ): Type {
+//     if (obj.length >= minimum) {
+//         return obj;
+//     } else {
+//         return { length: minimum }; // This doesn't work, it doesn't matter that it fits the constraint because it's not actually Type itself as defined by the parameter
+//     }
+// }
+// // If this worked you could do the following which is obviously wrong
+// // 'arr' gets value { length: 6 }
+// const arr = minimumLength([1, 2, 3], 6);
+// // and crashes here because arrays have
+// // a 'slice' method, but not the returned object!
+// console.log(arr.slice(0));
