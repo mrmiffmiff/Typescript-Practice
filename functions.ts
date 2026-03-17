585467bc32395bd5b7de7538afa83ce4d9d835de
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-03-17 12:51:00 -0700

    generics and inference within them

--- functions.ts
+++ functions.ts
@@ -49,3 +49,31 @@ function fn2(ctor: CallOrConstruct) {
     console.log(new ctor("10"));
 }
 fn2(Date);
+
+// Generics allow us to describe a correspondence between two values
+// For example an input type and the output type
+// Or two input types
+// Generics let us do this without actually predetermining the type, so we can avoid needing to use any
+// For example, returning the first element of an array
+function firstElement<Type>(arr: Type[]): Type | undefined {
+    return arr[0];
+}
+// s is of type 'string'
+const s = firstElement(["a", "b", "c"]);
+console.log(s)
+// n is of type 'number'
+const n = firstElement([1, 2, 3]);
+console.log(n);
+// u is of type undefined
+const u = firstElement([]);
+console.log(u);
+// Type didn't need specification, it can be inferred. Can also use multiple type parameters:
+function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
+    return arr.map(func);
+}
+// Parameter 'n' is of type 'string'
+// 'parsed' is of type 'number[]'
+const parsed = map(["1", "2", "3"], (n) => Number.parseInt(n));
+// I find this really cool how well the inference works tbh, the hints even changed as I typed that
+console.log(parsed);
+
