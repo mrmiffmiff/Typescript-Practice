a0dc0a5206c79f83ae582aec6fb2e9a089322061
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-03-19 10:35:05 -0700

    working with generic type variables

--- generics.js
+++ generics.js
@@ -1,18 +1,17 @@
 // Generics are a valuable tool for creating reusable components
 // The identity function returns what's passed in; we can use a type variable to capture the type of the arg so we can use it to denote the return
 // This is also more precise than just using 'any'
-function identity<Type>(arg: Type): Type {
+function identity(arg) {
     return arg;
 }
 // In many cases type inference can be done in calling
 let output1 = identity("test");
 // But passing the Type argument explicitly is always possible
-let output2 = identity<string>("test");
-
+let output2 = identity("test");
 // Compiler will enforce that Type Variables are treated properly
 // Above, you can't call .length on arg because Type isn't known to be a type that has said property
 // However, make it an array and it'll work
-function loggingIdentity<Type>(arg: Array<Type>): Array<Type> {
+function loggingIdentity(arg) {
     console.log(arg.length);
     return arg;
 }
